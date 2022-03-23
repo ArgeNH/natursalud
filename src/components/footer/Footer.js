b13f3dca11533834b05a30ea4b84dc90c52d8194
sergio1599 <sergioq15@hotmail.com>
2022-03-23 11:37:39 -0500

    fix principal page link

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const Footer = () => {
     return (
         <footer className="flex flex-col items-center justify-between px-6 py-4 bg-white dark:bg-gray-800 sm:flex-row mt-3">
-            <a href="#" className="text-xl font-bold text-gray-800 dark:text-white hover:text-gray-700 dark:hover:text-gray-300">Natursalud</a>
+            <Link to="/" className="text-xl font-bold text-gray-800 dark:text-white hover:text-gray-700 dark:hover:text-gray-300">Natursalud</Link>
 
             <p className="py-2 text-gray-800 dark:text-white sm:py-0">Todos los derechos reservados ©</p>
 
